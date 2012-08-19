bbe95067f78ebe6860021c8179a097692a0a654f
Thomas Friedel <thomas.friedel@berlin>
2012-08-19 10:23:59 +0200

    refactoring

--- TrackAnalyzer/DirectSkPostProc.java
+++ TrackAnalyzer/DirectSkPostProc.java
@@ -1,28 +1,30 @@
-/*************************************************************************
-
-  Copyright 2012 Ibrahim Sha'ath
-
-  This file is part of LibKeyFinder.
-
-  LibKeyFinder is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  LibKeyFinder is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with LibKeyFinder.  If not, see <http://www.gnu.org/licenses/>.
-
-*************************************************************************/
-
-/************************************************************************
- This file was modified/ported to Java in 2012 by Thomas Friedel
-************************************************************************/ 
-
+/**
+ * ***********************************************************************
+ *
+ * Copyright 2012 Ibrahim Sha'ath
+ *
+ * This file is part of LibKeyFinder.
+ *
+ * LibKeyFinder is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * LibKeyFinder is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * LibKeyFinder. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ************************************************************************
+ */
+/**
+ * **********************************************************************
+ * This file was modified/ported to Java in 2012 by Thomas Friedel
+***********************************************************************
+ */
 package TrackAnalyzer;
 
 import java.util.ArrayList;
@@ -43,28 +45,34 @@ public class DirectSkPostProc extends FftPostProcessor {
 // TODO check that last frequency doesn't go over Nyquist, and for sufficient low end resolution.
 		binOffsets = new int[bins];
 		mySpecKernel = new float[bins][fftFrameSize];
-		float myQFactor = (float) (params.getDirectSkStretch() * (Math.pow(2,(1.0 / params.getBpo()))-1));
-		for (int i = 0; i < bins; i++){
-		  float centreOfWindow = params.getBinFreq(i) * fftFrameSize / fr;
-		  float widthOfWindow = centreOfWindow * myQFactor;
-		  float beginningOfWindow = centreOfWindow - (widthOfWindow / 2);
-		  float endOfWindow = beginningOfWindow + widthOfWindow;
-		  float sumOfCoefficients = (float) 0.0;
-		  for (int thisFftBin = 0; thisFftBin < fftFrameSize; thisFftBin++){
-			if((float)thisFftBin < beginningOfWindow)
-			  continue; // haven't got to useful fft bins yet
-			if((float)thisFftBin > endOfWindow)
-			  break; // finished with useful fft bins
-			if(binOffsets[i] == 0)
-			  binOffsets[i] = thisFftBin; // first useful fft bin
-			float coefficient = kernelWindow(thisFftBin-beginningOfWindow,widthOfWindow);
-			sumOfCoefficients += coefficient;
-			mySpecKernel[i][thisFftBin] = coefficient;
-		  }
-		  // normalisation by sum of coefficients and frequency of bin; models CQT very closely
-		  int kernel_i_size = mySpecKernel[i].length;
-		  for (int j = 0; j < kernel_i_size; j++)
-			mySpecKernel[i][j] =  mySpecKernel[i][j] / sumOfCoefficients * params.getBinFreq(i);
+		float myQFactor = (float) (params.getDirectSkStretch() * (Math.pow(2, (1.0 / params.getBpo())) - 1));
+		for (int i = 0; i < bins; i++) {
+			float centreOfWindow = params.getBinFreq(i) * fftFrameSize / fr;
+			float widthOfWindow = centreOfWindow * myQFactor;
+			float beginningOfWindow = centreOfWindow - (widthOfWindow / 2);
+			float endOfWindow = beginningOfWindow + widthOfWindow;
+			float sumOfCoefficients = (float) 0.0;
+			ArrayList<Float> coefficients = new ArrayList<Float>();
+			for (int thisFftBin = 0; thisFftBin < fftFrameSize; thisFftBin++) {
+				if ((float) thisFftBin < beginningOfWindow) {
+					continue; // haven't got to useful fft bins yet
+				}
+				if ((float) thisFftBin > endOfWindow) {
+					break; // finished with useful fft bins
+				}
+				if (binOffsets[i] == 0) {
+					binOffsets[i] = thisFftBin; // first useful fft bin
+				}
+				float coefficient = kernelWindow(thisFftBin - beginningOfWindow, widthOfWindow);
+				sumOfCoefficients += coefficient;
+				coefficients.add(coefficient);
+			}
+			mySpecKernel[i] = Utils.floatArrayListToPrimitive(coefficients);
+			// normalisation by sum of coefficients and frequency of bin; models CQT very closely
+			int kernel_i_size = mySpecKernel[i].length;
+			for (int j = 0; j < kernel_i_size; j++) {
+				mySpecKernel[i][j] = mySpecKernel[i][j] / sumOfCoefficients * params.getBinFreq(i);
+			}
 		}
 
 	}
@@ -75,11 +83,11 @@ public class DirectSkPostProc extends FftPostProcessor {
 		for (int i = 0; i < bins; i++) {
 			float sum = (float) 0.0;
 			int kernel_i_size = mySpecKernel[i].length;
-		    float[] kernel_i = mySpecKernel[i];
+			float[] kernel_i = mySpecKernel[i];
 			for (int j = 0; j < kernel_i_size; j++) {
 				int binNum = binOffsets[i] + j;
-				double real = fftResult[binNum*2];
-				double imag = fftResult[binNum*2+1];
+				double real = fftResult[binNum * 2];
+				double imag = fftResult[binNum * 2 + 1];
 				float magnitude = (float) Math.sqrt((real * real) + (imag * imag));
 				sum += (magnitude * kernel_i[j]);
 			}
